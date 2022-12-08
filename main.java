fad81fed8c8ff30163969582f644dcd38e9ddd8b
Wizz <75508453+skw359@users.noreply.github.com>
2022-12-08 14:56:06 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
-public class CPUIntensive {
+public class CPUBurner {
   public static void main(String[] args) {
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100000; i++) {
       new Thread(() -> {
         while (true) {
           // Do nothing
